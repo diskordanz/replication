25d778d7d3bc73777c22a583a9bf2ebadab2b6ce
Nadya <nadybarsukova@gmail.com>
2019-03-11 08:47:53 +0300

    fix

--- main.go
+++ main.go
@@ -1,27 +1,37 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
+	"strconv"
 
-	_ "github.com/go-sql-driver/mysql" // Any sql.DB works
-	"github.com/tsenart/nap"
+	"database/sql"
+
+	rep "github.com/diskordanz/replication/rep"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
 
-var (
-	host     = "localhost"
-	port     = 3307
-	username = "admin"
-	password = "admin"
-)
+type Car struct {
+	Number         string
+	Model          string
+	Year           string
+	Mileage        string
+	InspectionDate string
+	Color          string
+}
+
+var db *sql.DB
 
 func main() {
-	// The first DSN is assumed to be the master and all
-	// other to be slaves
-	dsns := "tcp://user:password@master/dbname;"
-	dsns += "tcp://user:password@slave01/dbname;"
-	dsns += "tcp://user:password@slave02/dbname"
 
-	db, err := nap.Open("mysql", dsns)
+	dsns := "root:password@/master;"
+	dsns += "root:password@/slave01;"
+	dsns += "root:password@/slave02"
+
+	db, err := rep.Open("mysql", dsns)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -30,44 +40,185 @@ func main() {
 		log.Fatalf("Some physical database is unreachable: %s", err)
 	}
 
-	// Read queries are directed to slaves with Query and QueryRow.
-	// Always use Query or QueryRow for SELECTS
-	// Load distribution is round-robin only for now.
-	var count int
-	err = db.QueryRow("SELECT COUNT(*) FROM sometable").Scan(&count)
+	defer db.Close()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/cars", ListCars)
+	http.Handle("/", r)
+
+	fmt.Println("Server is listening...")
+	http.ListenAndServe(":8080", r)
+
+	/*
+		err = db.Exec(`CREATE TABLE IF NOT EXISTS cars(
+			car_id      integer,
+			model		varchar(32),
+			number		varchar(32),
+			year		varchar(32),
+			mileage		varchar(32),
+			date		varchar(32),
+			color		varchar(32));`)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = db.Exec(`INSERT INTO cars (car_id, model,number, year, mileage, date, color)
+		VALUES (1, 'dd', '333','1999', '44444','43/64', 'red'), (2, 'backy', '345245','2000', '10000','21/10', 'green');`)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var car Car
+		results, err := db.Query("SELECT model FROM cars")
+		if err != nil {
+			log.Fatal(err)
+		}
+		for results.Next() {
+			results.Scan(&car.Model)
+			log.Printf(car.Model)
+		}
+	*/
+}
+
+func ListCars(w http.ResponseWriter, r *http.Request) {
+
+	rows, err := db.Query("select model,number, year, mileage, date, color from cars")
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+	}
+	defer rows.Close()
+	cars := []Car{}
+
+	for rows.Next() {
+		p := Car{}
+		err := rows.Scan(&p.Model, &p.Number, &p.Year, &p.Mileage, &p.InspectionDate, &p.Color)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		cars = append(cars, p)
 	}
 
-	// Write queries are directed to the master with Exec.
-	// Always use Exec for INSERTS, UPDATES
-	_, err = db.Exec("UPDATE sometable SET something = 1")
 	if err != nil {
-		log.Fatal(err)
+		respondError(w, http.StatusInternalServerError, err.Error())
+		return
 	}
+	respondJSON(w, http.StatusOK, cars)
+}
 
-	// Prepared statements are aggregates. If any of the underlying
-	// physical databases fails to prepare the statement, the call will
-	// return an error. On success, if Exec is called, then the
-	// master is used, if Query or QueryRow are called, then a slave
-	// is used.
-	_, err = db.Prepare("SELECT * FROM sometable WHERE something = ?")
+func DeleteCar(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	_, err := db.Exec("delete from cars_db.cars where id = ?", id)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
 
-	// Transactions always use the master
-	tx, err := db.Begin()
+	http.Redirect(w, r, "/", 301)
+}
+
+func GetCar(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
 	if err != nil {
-		log.Fatal(err)
-	}
-	// Do something transactional ...
-	if err = tx.Commit(); err != nil {
-		log.Fatal(err)
+		respondError(w, http.StatusBadRequest, err.Error())
+		return
 	}
 
-	// If needed, one can access the master or a slave explicitly.
-	//master, slave := db.Master(), db.Slave()
-	_, _ = db.Master(), db.Slave()
+	row, err := db.Query("select * from cars where id = ?", id)
+	car := Car{}
+	err = row.Scan(&car.Model, &car.Number, &car.Year, &car.Mileage, &car.InspectionDate, &car.Color)
+	if err != nil {
+		respondError(w, http.StatusNotFound, err.Error())
+		return
+	}
 
+	respondJSON(w, http.StatusOK, car)
+
+}
+
+func EditCar(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Println(err)
+	}
+	//id := r.FormValue("id")
+	//model := r.FormValue("model")
+	//company := r.FormValue("company")
+	//price := r.FormValue("price")
+
+	//_, err = database.Exec("update productdb.Products set model=?, company=?, price = ? where id = ?",
+	//	model, company, price, id)
+
+	if err != nil {
+		log.Println(err)
+	}
+	http.Redirect(w, r, "/", 301)
+}
+
+func CreateCar(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+
+		err := r.ParseForm()
+		if err != nil {
+			log.Println(err)
+		}
+		//model := r.FormValue("model")
+		//company := r.FormValue("company")
+		//price := r.FormValue("price")
+
+		//_, err = database.Exec("insert into productdb.Products (model, company, price) values (?, ?, ?)",
+		//	model, company, price)
+
+		if err != nil {
+			log.Println(err)
+		}
+		http.Redirect(w, r, "/", 301)
+	} else {
+		http.ServeFile(w, r, "templates/create.html")
+	}
+}
+
+func Insert(car *Car) {
+
+	stmt, err := db.Prepare(`INSERT INTO cars(
+			model,
+			number,			
+			year,		
+			mileage,		
+			date,		
+			color) VALUES(?, ?, ?, ?, ?, ?)`)
+	if err != nil {
+		panic(err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(
+		&car.Model,
+		&car.Number,
+		&car.Year,
+		&car.Mileage,
+		&car.InspectionDate,
+		&car.Color)
+	if err != nil {
+		panic(err)
+	}
+	log.Println("insert successful")
+
+}
+
+func respondJSON(w http.ResponseWriter, status int, payload interface{}) {
+	response, err := json.Marshal(payload)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write([]byte(response))
+}
+
+func respondError(w http.ResponseWriter, code int, message string) {
+	respondJSON(w, code, map[string]string{"error": message})
 }
---
+++ rep/rep.go
@@ -0,0 +1,157 @@
+package rep
+
+import (
+	"database/sql"
+	"database/sql/driver"
+	"strings"
+	"sync/atomic"
+	"time"
+)
+
+type DB struct {
+	pdbs  []*sql.DB
+	count uint64
+}
+
+func Open(driverName, dataSourceNames string) (*DB, error) {
+	conns := strings.Split(dataSourceNames, ";")
+	db := &DB{pdbs: make([]*sql.DB, len(conns))}
+	err := scatter(len(db.pdbs), func(i int) (err error) {
+		db.pdbs[i], err = sql.Open(driverName, conns[i])
+		return err
+	})
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func (db *DB) Close() error {
+	return scatter(len(db.pdbs), func(i int) error {
+		return db.pdbs[i].Close()
+	})
+}
+
+func (db *DB) Driver() driver.Driver {
+	return db.pdbs[0].Driver()
+}
+
+func (db *DB) Begin() (*sql.Tx, error) {
+	return db.pdbs[0].Begin()
+}
+
+func (db *DB) Exec(query string, args ...interface{}) error {
+	scatter(len(db.pdbs), func(i int) error {
+		db.pdbs[i].Exec(query, args...)
+		return nil
+	})
+	return nil
+}
+
+func (db *DB) Sync(query string, args ...interface{}) error {
+	scatter(len(db.pdbs)-1, func(i int) error {
+		db.pdbs[i+1].Exec(query, args...)
+		return nil
+	})
+	return nil
+}
+
+func (db *DB) Ping() error {
+	return scatter(len(db.pdbs), func(i int) error {
+		return db.pdbs[i].Ping()
+	})
+}
+func (db *DB) Prepare(query string) (Stmt, error) {
+	stmts := make([]*sql.Stmt, len(db.pdbs))
+
+	err := scatter(len(db.pdbs), func(i int) (err error) {
+		stmts[i], err = db.pdbs[i].Prepare(query)
+		return err
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &stmt{db: db, stmts: stmts}, nil
+}
+func (db *DB) Query(query string, args ...interface{}) (*sql.Rows, error) {
+	return db.pdbs[db.slave(len(db.pdbs))].Query(query, args...)
+}
+func (db *DB) QueryRow(query string, args ...interface{}) *sql.Row {
+	return db.pdbs[db.slave(len(db.pdbs))].QueryRow(query, args...)
+}
+func (db *DB) SetMaxIdleConns(n int) {
+	for i := range db.pdbs {
+		db.pdbs[i].SetMaxIdleConns(n)
+	}
+}
+func (db *DB) SetMaxOpenConns(n int) {
+	for i := range db.pdbs {
+		db.pdbs[i].SetMaxOpenConns(n)
+	}
+}
+func (db *DB) SetConnMaxLifetime(d time.Duration) {
+	for i := range db.pdbs {
+		db.pdbs[i].SetConnMaxLifetime(d)
+	}
+}
+func (db *DB) Slave() *sql.DB {
+	return db.pdbs[db.slave(len(db.pdbs))]
+}
+func (db *DB) Master() *sql.DB {
+	return db.pdbs[0]
+}
+
+func (db *DB) slave(n int) int {
+	if n <= 1 {
+		return 0
+	}
+	return int(1 + (atomic.AddUint64(&db.count, 1) % uint64(n-1)))
+}
+
+func scatter(n int, fn func(i int) error) error {
+	errors := make(chan error, n)
+
+	var i int
+	for i = 0; i < n; i++ {
+		go func(i int) { errors <- fn(i) }(i)
+	}
+
+	var err, innerErr error
+	for i = 0; i < cap(errors); i++ {
+		if innerErr = <-errors; innerErr != nil {
+			err = innerErr
+		}
+	}
+
+	return err
+}
+
+type Stmt interface {
+	Close() error
+	Exec(...interface{}) (sql.Result, error)
+	Query(...interface{}) (*sql.Rows, error)
+	QueryRow(...interface{}) *sql.Row
+}
+type stmt struct {
+	db    *DB
+	stmts []*sql.Stmt
+}
+
+func (s *stmt) Close() error {
+	return scatter(len(s.stmts), func(i int) error {
+		return s.stmts[i].Close()
+	})
+}
+func (s *stmt) Exec(args ...interface{}) (sql.Result, error) {
+	return s.stmts[0].Exec(args...)
+}
+
+func (s *stmt) Query(args ...interface{}) (*sql.Rows, error) {
+	return s.stmts[s.db.slave(len(s.db.pdbs))].Query(args...)
+}
+
+func (s *stmt) QueryRow(args ...interface{}) *sql.Row {
+	return s.stmts[s.db.slave(len(s.db.pdbs))].QueryRow(args...)
+}
